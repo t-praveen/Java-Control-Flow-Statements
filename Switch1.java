0a5408a116b72c64675c53b1adc558355b35a881
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-06-14 16:01:38 +0530

    Add files via upload

---
+++ Switch1.java
@@ -0,0 +1,37 @@
+package Recursion;
+import java.util.Scanner ;
+
+public class Switch1 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a Character : ");
+		char ch = sc.next().charAt(0);
+		Test(ch);
+		sc.close();
+	}
+	public static void Test(char ch)
+	{
+		switch(ch)
+		{
+		case 'a' : case 'A' :
+			System.out.println("Found A or a .");
+			break;
+		case 'b' : case 'B' :
+			System.out.println("Found B or b .");
+			break;
+		case 'c' : case 'C' :
+			System.out.println("Found C or c .");
+			break;
+		case 'd' : case 'D' :
+			System.out.println("Found D or d .");
+			break;
+		case 'e' : case 'E' :
+			System.out.println("Found E or e .");
+			break;
+		default : System.out.println("Not found !! ");
+		}
+	}
+
+}
