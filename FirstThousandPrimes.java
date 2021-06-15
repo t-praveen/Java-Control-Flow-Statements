bf3e717c8a5092015337681bd033280b55b3bf90
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-06-15 13:54:16 +0530

    Add files via upload

---
+++ FirstThousandPrimes.java
@@ -0,0 +1,15 @@
+package Recursion;
+
+public class FirstThousandPrimes {
+
+	public static void main(String[] args) {
+		int i = 0 ;
+		prime(i);
+	}
+	public static void prime(int i)
+	{
+		for(i = 0 ; i <= 1000 ; i++)
+			System.out.println(i);
+	}
+
+}
