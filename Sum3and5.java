a086ac4e9e1dbaaa1d3d3c8577f26162960c565b
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-06-15 14:53:45 +0530

    Add files via upload

---
+++ Sum3and5.java
@@ -0,0 +1,28 @@
+package Recursion;
+
+public class Sum3and5 {
+
+	public static void main(String[] args) {
+		
+		int count = 0 ; 
+		int sum = 0 ;
+		Sum(count , sum);
+	}
+	public static void Sum(int count , int sum)
+	{
+		for(int i = 1 ; i <= 1000 ; i++)
+		{
+			if(i % 3 == 0 && i % 5 == 0)
+			{
+				count++ ;
+				sum += i ;
+				System.out.println("Found number : "+i);
+			}
+			if(count == 5)
+				break ;
+		}
+		System.out.println("Sum is : "+sum);
+
+	}
+
+}
