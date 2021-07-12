07d707ea2fff6969d9e92ab0f0378c3ea9f70612
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-07-12 13:24:07 +0530

    Add files via upload

---
+++ WhileLoopExampleProgram2.java
@@ -0,0 +1,40 @@
+package loops;
+
+public class WhileLoopExampleProgram2 {
+
+	public static void main(String[] args) {
+		
+
+			int number = 5;
+			int finishNumber = 20;
+			int evenNumbersFound = 0;
+			while(number <= finishNumber) {
+            if(!isEvenNumber(number)) {
+                number++;
+                continue;
+            }
+
+            System.out.println("Even number " + number);
+            number++;
+
+            evenNumbersFound++;
+            if(evenNumbersFound >=5) {
+                break;
+            }
+        }
+
+        System.out.println("Total even numbers found = " + evenNumbersFound);
+
+
+    }
+
+    public static boolean isEvenNumber(int number) {
+        if((number % 2)  == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+}
